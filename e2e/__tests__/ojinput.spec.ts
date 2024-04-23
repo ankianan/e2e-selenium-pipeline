d200cba586832a9aea7037f08ccd4ea9f2f5e919
Ankit Anand <ankit.anand@oracle.com>
2024-04-23 15:23:29 +0530

    chore: add visibility check before element

--- e2e/__tests__/ojinput.spec.ts
+++ e2e/__tests__/ojinput.spec.ts
@@ -14,13 +14,15 @@ describe("Test the WebElement for oj-input-text", function () {
 
   it.only("check Value Property", async function () {
     let ojWebElement = await driver.findElement(By.id("text-input"));
+    await driver.wait(until.elementIsVisible(ojWebElement));
+    expect(await ojWebElement.getAttribute("value")).toBe("Green");
 
     expect(await driver.executeScript(`
       const [element] = arguments;
       return element.getProperty('value');
     `, ojWebElement)).toBe("Green");
 
-    await driver.wait(until.elementIsVisible(ojWebElement));
+    
 
     let nativeElement = await ojWebElement.findElement(By.css("#text-input input"));
     expect(await nativeElement.getAttribute("value")).toBe("Green");
