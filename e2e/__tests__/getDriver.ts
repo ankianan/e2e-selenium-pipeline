1f4f63f13ee109a8d277cd274a3f9bc2cab9110c
Ankit Anand <ankit.anand@oracle.com>
2024-04-23 15:03:42 +0530

    chore: headless

--- e2e/__tests__/getDriver.ts
+++ e2e/__tests__/getDriver.ts
@@ -1,8 +1,12 @@
 import { WebDriver, Builder, Browser } from "selenium-webdriver";
-
+import chrome from 'selenium-webdriver/chrome';
 export async function getDriver() {
+  const chromOptions = new chrome.Options();
+    chromOptions.addArguments('--headless=new');
+                            
   let driver = await new Builder()
     .forBrowser(Browser.CHROME)
+    .setChromeOptions(chromOptions)
     .build();
   return driver;
 }
